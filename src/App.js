2ceee0c1d05e979f4402bf7c27bada9acda0d0db
Paul Hallett <paul.hallett@sharesies.co.nz>
2023-05-19 11:46:43 +1200

    set up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Beckett software
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="App-link pt"
+          href="https://github.com/beckett-software"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/Github_black.svg" alt="github" />
         </a>
       </header>
     </div>

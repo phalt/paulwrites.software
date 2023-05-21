8c56c5a935ff2fd47cba7e1da4e1f0f1aa9b5efb
Paul Hallett <paul.hallett@sharesies.co.nz>
2023-05-22 10:04:13 +1200

    new framework link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import logo from './logo.jpeg';
 import './App.css';
 
+const projects = [
+  {name: "Beckett Framework", 
+  url: "https://github.com/beckett-software/beckett-framework", 
+  image: "https://github.com/beckett-software/beckett-framework/blob/main/docs/banner.jpg?raw=true",
+  description: <>
+    <p>A full stack application server in Python and TypeScript, with strongly-linked types between frontend and backend.</p>
+    <p>Beckett framework boosts productivity by auto-generating Types, React pages, API clients, and more.</p>
+  </>
+},
+]
+
+const ProjectBanner = ({name, url, image, description}) =>
+    <div className='projectBanner'>
+    <a href={url}><h4>{name}</h4></a>
+      <img src={image} alt={name} />
+      {description}
+    </div>
+
 function App() {
   return (
     <div className="App">
@@ -10,9 +28,7 @@ function App() {
         </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <h4>Projects</h4>
-        <a href="https://github.com/beckett-software/beckett-framework">
-          <img src="https://gh-card.dev/repos/beckett-software/beckett-framework.png" alt="beckett-framework" />
-        </a>
+          {projects.map(p => <ProjectBanner {...p} />)}
         <p><a href="https://beckettsoftware.substack.com/" target="_blank" rel="noopener noreferrer">Articles</a></p>
         <a
           className="App-link pt"

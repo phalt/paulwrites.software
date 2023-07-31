e10f34fac18b57ed8901b7f48aaee6aa07156102
Paul Hallett <paul.hallett@sharesies.co.nz>
2023-07-31 14:37:58 +1200

    corrections, updates etc

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 
 const projects = [
   {name: "Clientele", 
-  url: "https://github.com/beckett-software/clientele", 
-  image: "https://github.com/beckett-software/clientele/blob/main/docs/clientele.jpeg?raw=true",
+  url: "https://github.com/phalt/clientele", 
+  image: "https://github.com/phalt/clientele/blob/main/docs/clientele.jpeg?raw=true",
   description: <>
-    <p>Typed API Clients from OpenAPI specs</p>
+    <p>Typed API Clients from OpenAPI schemas</p>
   </>
 },
-  {name: "Beckett Framework", 
-  url: "https://github.com/beckett-software/beckett-framework", 
-  image: "https://github.com/beckett-software/beckett-framework/blob/main/docs/banner.jpg?raw=true",
+  {name: "react-flask", 
+  url: "https://github.com/phalt/react-flask", 
+  image: "https://github.com/phalt/react-flask/blob/main/docs/banner.jpg?raw=true",
   description: <>
-    <p>The Efficient Framework for Building Type-Safe Python Web Servers and React TypeScript Websites</p>
+    <p>Fully typed Flask and React applications</p>
   </>
 },
 ]
@@ -30,22 +30,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Beckett software
+          Paul writes software
         </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <h4>Projects</h4>
           {projects.map(p => <ProjectBanner {...p} />)}
-        <p><a href="https://beckettsoftware.substack.com/" target="_blank" rel="noopener noreferrer">Articles</a></p>
         <a
           className="App-link pt"
-          href="https://github.com/beckett-software"
+          href="https://github.com/phalt"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img src="https://raw.githubusercontent.com/gauravghongde/social-icons/9d939e1c5b7ea4a24ac39c3e4631970c0aa1b920/SVG/Black/Github_black.svg" alt="github" />
         </a>
       </header>
-      <p>Beckett software and all related projects is copyright Paul Hallett 2023</p>
+      <p>"Paul writes software" and all related projects is copyright Paul Hallett 2023</p>
     </div>
   );
 }
